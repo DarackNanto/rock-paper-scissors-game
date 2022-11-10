5fb35eaf23e8c94b39e71651049591d5759c66b3
Darack MBP <darackn@gmail.com>
2022-11-10 08:28:42 -0600

    Create basic function call playround to play one round and test logic Function playround compare the computer random input vs the user input.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Function generates random computer choice between Rock, paper and scissor
 function getComputerChoice() {
   const choices = ["Rock", "Paper", "Scissors"];
   let i = Math.floor(Math.random() * 3);
@@ -5,4 +6,27 @@ function getComputerChoice() {
   return choice;
 }
 
-console.log(getComputerChoice());
+//Function compares player vs computer input. Then output string result.
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a tie! Try again!";
+  } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+    return "You Lose! Paper beats Rock";
+  } else {
+    return "Logic still under construction!";
+  }
+}
+
+//Prompt to get player input choice
+const playerInputChoice = prompt("Enter Rock, Paper or Scissors", "Rock");
+
+//Makes player input case insensitive. Capitalize first letter and lowercase remaining letters
+const playerSelection =
+  playerInputChoice[0].toUpperCase() + playerInputChoice.slice(1).toLowerCase();
+console.log(playerSelection);
+
+//Assign random computer choice to ComputerSelection variable
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+
+console.log(playRound(playerSelection, computerSelection));
