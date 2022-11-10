4ed1d851f3103ffda328b254c95609ac9e62916c
Darack MBP <darackn@gmail.com>
2022-11-10 10:38:54 -0600

    Add function game. Game function plays 5 rounds and stop. Add score keeping per round. Incremental of 1 point. Modify player input prompt into a function, to make it reusable and.

--- script.js
+++ script.js
@@ -7,36 +7,54 @@ function getComputerChoice() {
 }
 
 //Function compares player vs computer input. Then return string result.
+let playerScore = 0;
+let computerScore = 0;
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return "It's a tie! Try again!";
   } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+    computerScore += 1;
     return "You Lose! Paper beats Rock";
   } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    playerScore += 1;
     return "You Win! Rock beats Scissors";
   } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    computerScore += 1;
     return "You Lose! Scissors beats Paper";
   } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    playerScore += 1;
     return "You Win! Paper beats Rock";
   } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    playerScore += 1;
     return "You Win! Scissors beats Paper";
   } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    computerScore += 1;
     return "You Lose! Rock beats Scissors";
   } else {
     return "Error in selection. Try Again!";
   }
 }
 
-//Prompt to get player input choice
-const playerInputChoice = prompt("Enter Rock, Paper or Scissors", "rock");
+function playerChoice() {
+  //Prompt to get player input choice
+  const playerInputChoice = prompt("Enter Rock, Paper or Scissors", "rock");
+  //Makes player input case insensitive. Capitalize first letter and lowercase remaining letters
+  const choice =
+    playerInputChoice[0].toUpperCase() +
+    playerInputChoice.slice(1).toLowerCase();
+  return choice;
+}
 
-//Makes player input case insensitive. Capitalize first letter and lowercase remaining letters
-const playerSelection =
-  playerInputChoice[0].toUpperCase() + playerInputChoice.slice(1).toLowerCase();
-console.log(playerSelection);
-
-//Assign random computer choice to ComputerSelection variable
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-
-console.log(playRound(playerSelection, computerSelection));
+//Plays 5 rounds of stop
+function game() {
+  for (let i = 0; i < 5; i++) {
+    //Assign random player choice to playerSelection variable
+    const playerSelection = playerChoice();
+    //Assign random computer choice to ComputerSelection variable
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playerScore, computerScore);
+  }
+}
+gam;
+e();
