38f06ab05ed041a059684e0c571e1d221792005c
Darack MBP <darackn@gmail.com>
2022-11-10 08:55:07 -0600

    Add all possible outcome to rock, paper and scissor result to playRound Function. Any option from the computer and player will return a win, loss or tie result.

--- script.js
+++ script.js
@@ -6,19 +6,29 @@ function getComputerChoice() {
   return choice;
 }
 
-//Function compares player vs computer input. Then output string result.
+//Function compares player vs computer input. Then return string result.
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return "It's a tie! Try again!";
   } else if (playerSelection === "Rock" && computerSelection === "Paper") {
     return "You Lose! Paper beats Rock";
+  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    return "You Win! Rock beats Scissors";
+  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    return "You Lose! Scissors beats Paper";
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    return "You Win! Paper beats Rock";
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    return "You Win! Scissors beats Paper";
+  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    return "You Lose! Rock beats Scissors";
   } else {
-    return "Logic still under construction!";
+    return "Error in selection. Try Again!";
   }
 }
 
 //Prompt to get player input choice
-const playerInputChoice = prompt("Enter Rock, Paper or Scissors", "Rock");
+const playerInputChoice = prompt("Enter Rock, Paper or Scissors", "rock");
 
 //Makes player input case insensitive. Capitalize first letter and lowercase remaining letters
 const playerSelection =
